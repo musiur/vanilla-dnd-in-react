30e1bf02dfd34adc3fa37f56926301f2c69c9e3d
musiur <musiur.opu@gmail.com>
2023-08-05 10:20:34 +0600

    comp-dev:: header: Brand name modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="stikcy z-50 top-0 backdrop-blur bg-white/30 p-4">
       <nav className="flex justify-between items-center container">
-        <span className="font-black text-2xl">DND</span>
+        <span className="font-black text-2xl">DND@musiur</span>
         <a href="https://github.com/musiur/vanilla-dnd-in-react" className="hover:underline">SourceCode</a>
       </nav>
     </header>
