dc308f5f136ac6d30d9eb466c4ba75f968a46fce
musiur <musiur.opu@gmail.com>
2023-08-24 17:22:18 +0600

    homepage text hided

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,12 @@ const App = () => {
       <div className="hidden lg:block">
         <Header />
         <KanbanBody />
-        <p className="text-9xl container section">
+        {/* <p className="text-9xl container section">
           Commodo cupidatat duis quis nisi. Dolor qui enim labore quis laborum
           quis laborum anim sint reprehenderit. Do ullamco pariatur in aute est.
           Ullamco non minim consectetur nostrud. Velit commodo fugiat commodo ea
           sit consectetur incididunt.
-        </p>
+        </p> */}
       </div>
       <div className="h-screen flex items-center justify-center text-3xl font-semibold text-red-400 block lg:hidden">
         Please open in Desktop full view
