85ad98c8a52976edd17db993e57a5108b76e300b
musiur <musiur.opu@gmail.com>
2023-08-24 10:10:10 +0600

    comp-dev:: navbar: removed extra sticky bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,16 @@
 const Header = () => {
   return (
-    <>
-      <header className="stikcy z-50 top-0 backdrop-blur p-4 bg-purple-400">
-        <nav className="flex justify-between items-center container ">
-          <span className="font-black text-2xl">DND@musiur</span>
-          <a
-            href="https://github.com/musiur/vanilla-dnd-in-react"
-            className="hover:underline"
-          >
-            SourceCode
-          </a>
-        </nav>
-      </header>
-      <div className="py-5 bg-gray-50 sticky top-0"></div>
-    </>
+    <header className="stikcy z-50 top-0 backdrop-blur p-4 bg-purple-400">
+      <nav className="flex justify-between items-center container ">
+        <span className="font-black text-2xl">DND@musiur</span>
+        <a
+          href="https://github.com/musiur/vanilla-dnd-in-react"
+          className="hover:underline"
+        >
+          SourceCode
+        </a>
+      </nav>
+    </header>
   );
 };
 
